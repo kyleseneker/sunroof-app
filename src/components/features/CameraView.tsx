b698cc2f4576be06c550bfdd7ff8ad0050833990
kyleseneker <kyle.seneker@gmail.com>
2026-01-02 02:37:25 -0600

    style: remove misleading viewfinder frame overlay

--- src/components/features/CameraView.tsx
+++ src/components/features/CameraView.tsx
@@ -627,15 +627,6 @@ const handleAudioError = (message: string) => {
               </div>
             )}
             
-            {/* Corner frame guides - only show when camera is ready */}
-            {cameraReady && (
-              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-72 h-72 pointer-events-none z-10">
-                <div className="absolute top-0 left-0 w-10 h-10 border-l-2 border-t-2 border-white/40 rounded-tl-2xl" />
-                <div className="absolute top-0 right-0 w-10 h-10 border-r-2 border-t-2 border-white/40 rounded-tr-2xl" />
-                <div className="absolute bottom-0 left-0 w-10 h-10 border-l-2 border-b-2 border-white/40 rounded-bl-2xl" />
-                <div className="absolute bottom-0 right-0 w-10 h-10 border-r-2 border-b-2 border-white/40 rounded-br-2xl" />
-              </div>
-            )}
             
             {/* Camera error state */}
             {cameraError && (
